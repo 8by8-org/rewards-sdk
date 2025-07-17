9a0228b8b161777dbb083d905fdd2af2f3d15f48
Joseph Dvorak <dvorak@Josephs-iMac.local>
2025-07-17 01:15:40 -0700

    converted get methods to GET requests

--- src/rpc-service/rewards-client.ts
+++ src/rpc-service/rewards-client.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { object, z } from "zod";
 import { BaseServer } from "./base-server";
 import { contextualizedRewardSchema, voucherSchema } from "../schema";
 import type {
@@ -23,58 +23,68 @@ export class RewardsClient implements IRewardsService {
   }
 
   async getRewards(opts?: GetRewardsOpts): Promise<IContextualizedReward[]> {
-    const data = await this.executeRemoteProcedure(
-      BaseServer.ROUTES.getRewards,
-      opts
-    );
+    let endpoint = this.apiUrl + "/" + BaseServer.ROUTES.getRewards;
+
+    if (opts && Object.keys(opts).length > 1) {
+      const queryParams: string[] = [];
+
+      Object.entries(opts).forEach(([opt, value]) => {
+        if (typeof value === "object") {
+          value = JSON.stringify(value);
+        }
+        opt = encodeURIComponent(opt);
+        value = encodeURIComponent(value);
+        const queryParam = opt + "=" + value;
+        queryParams.push(queryParam);
+      });
+
+      const queryString = "?" + queryParams.join("&");
+      endpoint += queryString;
+    }
+
+    const request = this.createAPIRequest("GET");
+    const response = await fetch(endpoint, request);
+    const data = await response.json();
     const parsed = contextualizedRewardSchema.array().parse(data);
     return parsed;
   }
 
   async getAllRewardCategories(): Promise<string[]> {
-    const data = await this.executeRemoteProcedure(
-      BaseServer.ROUTES.getAllRewardCategories
-    );
+    const endpoint =
+      this.apiUrl + "/" + BaseServer.ROUTES.getAllRewardCategories;
+    const request = this.createAPIRequest("GET");
+    const response = await fetch(endpoint, request);
+    const data = await response.json();
     const parsed = z.string().array().parse(data);
     return parsed;
   }
 
   async claimReward(rewardId: string): Promise<IVoucher[]> {
-    const data = await this.executeRemoteProcedure(
-      BaseServer.ROUTES.claimReward,
-      {
-        rewardId,
-      }
-    );
+    const endpoint = this.apiUrl + "/" + BaseServer.ROUTES.claimReward;
+    const request = this.createAPIRequest("POST", {
+      rewardId,
+    });
+    const response = await fetch(endpoint, request);
+    const data = await response.json();
     const parsed = voucherSchema.array().parse(data);
     return parsed;
   }
 
-  private async executeRemoteProcedure(
-    route: (typeof BaseServer.ROUTES)[keyof typeof BaseServer.ROUTES],
-    body?: object
-  ) {
-    const endpoint = this.apiUrl + "/" + route;
-
-    const requestParams: RequestInit = {
-      method: "POST",
+  private createAPIRequest(method: string, body?: object): RequestInit {
+    const request: RequestInit = {
+      method,
     };
 
     if (this.apiKey) {
-      const authHeaderValue =
-        BaseServer.AUTHORIZATION_SCHEME + " " + this.apiKey;
-
-      requestParams.headers = {
-        [BaseServer.AUTHORIZATION_HEADER]: authHeaderValue,
+      request.headers = {
+        [BaseServer.AUTHORIZATION_HEADER]: `${BaseServer.AUTHORIZATION_SCHEME} ${this.apiKey}`,
       };
     }
 
     if (body) {
-      requestParams.body = JSON.stringify(body);
+      request.body = JSON.stringify(body);
     }
 
-    const response = await fetch(endpoint, requestParams);
-    const data = await response.json();
-    return data;
+    return request;
   }
 }
--- src/schema/get-rewards-params-schema.ts
+++ src/schema/get-rewards-params-schema.ts
@@ -6,8 +6,18 @@ import { rewardsCursorSchema } from "./rewards-cursor-schema";
 export const getRewardsParamsSchema = z.object({
   redemptionForumFilter: z.enum(RedemptionForumFilter).optional(),
   sortOrder: z.enum(SortOrder).optional(),
-  userCoordinates: pointSchema.optional(),
-  cursor: rewardsCursorSchema.optional(),
+  userCoordinates: pointSchema
+    .or(z.string())
+    .optional()
+    .transform((v) =>
+      typeof v === "string" ? pointSchema.parse(JSON.parse(v)) : v
+    ),
+  cursor: rewardsCursorSchema
+    .or(z.string())
+    .optional()
+    .transform((v) =>
+      typeof v === "string" ? rewardsCursorSchema.parse(JSON.parse(v)) : v
+    ),
   maxDistance: z.number().optional(),
   categories: z.string().array().optional(),
   ignoreMaxDistanceForOnlineRewards: z.boolean().optional(),
